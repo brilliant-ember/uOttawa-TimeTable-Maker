84de2d55f6204b08655bbfdd19dd380731988ac8
Richard Hayes <richardhayes@Richards-MacBook-Pro.local>
2017-05-05 12:21:44 -0400

    added CourseBuilder class to convert url to json or list formatted data. Added table UI html file to demo rendering of timetable

--- Initial.py
+++ Initial.py
@@ -1,19 +1,13 @@
-##WebScraper lxml
-#use chcp 65001 in terminal in case of an encoding error
+#
+# WebScraper
+#
 from urllib.request import urlopen
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 
-url="https://pythonprogramming.net/parsememcparseface/"
-client=urlopen(url).read()
-soup=BeautifulSoup(client, "lxml")
+url = "https://pythonprogramming.net/parsememcparseface/"
+client = urlopen(url).read().decode("uft-8")
+soup = BeautifulSoup(client, "lxml")
 
-#print(soup.find_all("p")) #finds all object p tags in the page{paragraphs}
-
-#for paragraph in soup.find_all("p"): 
- #   print (paragraph.text) # only prints paragraphs not all texts
-    
-    
-    
 #prints all text in the page even if it's not in paragraph tags
 print(soup.get_text())
 
