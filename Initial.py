1a46f34b91739f153be086fb501fd2b78be66f50
brilliant-ember <fire.senfony@gmail.com>
2017-05-07 07:39:29 -0600

    extracts table data from the uni website

--- Initial.py
+++ Initial.py
@@ -1,6 +1,11 @@
 ##WebScraper Selenium
-import selenium
+from selenium import webdriver
 
 path = "D:\chromedriver.exe"
 browser = webdriver.Chrome(path)
 browser.get("https://web30.uottawa.ca/v3/SITS/timetable/Course.aspx?id=018041&term=2175&session=A")
+#browser.find_element_by_xpath("""//*[@id="1"]""")
+rows = browser.find_elements_by_class_name("display")
+for i in rows:
+    print(i.text)
+    #oneRow=[x.text for x in i]
