6c35e38dcc367d41f7c6649bdf789e21c7aa9e1f
brilliant-ember <fire.senfony@gmail.com>
2017-05-07 07:43:34 -0600

    extracts data from school website with selenium

--- Initial.py
+++ Initial.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 path = "D:\chromedriver.exe"
 browser = webdriver.Chrome(path)
 browser.get("https://web30.uottawa.ca/v3/SITS/timetable/Course.aspx?id=018041&term=2175&session=A")
-#browser.find_element_by_xpath("""//*[@id="1"]""")
+#browser.find_element_by_xpath("""//*[@id="1"]""") #
 rows = browser.find_elements_by_class_name("display")
 for i in rows:
     print(i.text)
