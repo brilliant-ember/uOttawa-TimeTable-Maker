9d9420d8fe6c1e68380b81eba2c7dd44a7dd1d4b
brilliant-ember <fire.senfony@gmail.com>
2017-05-11 13:38:19 -0600

    puts columns in a list, exept for locaton and prof

--- Initial.py
+++ Initial.py
@@ -4,8 +4,53 @@ from selenium import webdriver
 path = "D:\chromedriver.exe"
 browser = webdriver.Chrome(path)
 browser.get("https://web30.uottawa.ca/v3/SITS/timetable/Course.aspx?id=018041&term=2175&session=A")
-#browser.find_element_by_xpath("""//*[@id="1"]""") #
-rows = browser.find_elements_by_class_name("display")
-for i in rows:
-    print(i.text)
-    #oneRow=[x.text for x in i]
+table = browser.find_element_by_xpath("""//*[@id="1"]""") 
+#ObjRows = table.find_elements_by_tag_name("div")
+
+
+Section=[]
+Activity=[]
+Day=[]
+Location=[]
+Prof=[]
+
+
+act = table.find_elements_by_class_name("Activity")
+sec = table.find_elements_by_class_name("Section")
+day = table.find_elements_by_class_name("Day")
+loc = table.find_elements_by_class_name("Place")
+prof = table.find_elements_by_class_name("Professor")
+
+
+
+
+
+for a, s, d, l, p  in zip(act, sec, day, loc, prof):
+    
+    #print(i.text, " Of Type= ",type(i.text))
+    Activity.append(a.text)
+    Section.append(s.text)
+    Day.append(d.text)
+    
+
+
+    
+print("_______________________Day______________________________")
+print(Day)
+print("________________________Act_____________________________\n")
+print(Activity)
+print("_______________________Sec______________________________\n")
+print(Section)
+print("_______________________Loc______________________________\n")
+print(Location)
+print("________________________Prof_____________________________\n")
+print(Prof)
+
+
+
+    
+    
+    
+    
+    
+    
