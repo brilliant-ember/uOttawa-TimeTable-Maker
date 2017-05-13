c8a02106b9117fed6192c96f4cd588bdae14d3f1
brilliant-ember <fire.senfony@gmail.com>
2017-05-13 06:19:47 -0600

    Makes the program cabable of identifying multiple sections

--- Initial.py
+++ Initial.py
@@ -31,42 +31,44 @@ class Course:
         Location=self.Location
         Prof=self.Prof
         Time=self.Time
+        tmp=[]
 
         browser = webdriver.Chrome(path)
         browser.get(url)
-        table = browser.find_element_by_xpath("""//*[@id="1"]""") 
-        #ObjRows = table.find_elements_by_tag_name("div")
-
-
-        act = table.find_elements_by_class_name("Activity")
-        sec = table.find_elements_by_class_name("Section")
-        day = table.find_elements_by_class_name("Day")
-        loc = table.find_elements_by_class_name("Place")
-        prof = table.find_elements_by_class_name("Professor")
-
-
-        #this loop fills the empty lists with there respective elements from the school's website
-        for a, s, d, l, p  in zip(act, sec, day, loc, prof):
-
-            #print(i.text, " Of Type= ",type(i.text))
-            Activity.append(a.text)
-            Section.append(s.text)
-            Day.append(d.text)
-            Prof.append(p.text)
-            Location.append(l.text)
-
-        #loop to split the actual day from time in the Day list
-        tmp=[]
-
-        for x in Day:
-            y = x.split(" ")
-            Time.append(y[1]+","+y[-1])
-            tmp = tmp+[y[0]]
-
-        self.Day = tmp[:] # copy tmp's elements
-        #tmp = [] #Set tmp to an empty list
+        #table = browser.find_element_by_xpath("""//*[@id="1"]""") 
+        tables = browser.find_elements_by_id("schedule")
         
-       '''
+        for table in tables:
+
+            act = table.find_elements_by_class_name("Activity")
+            sec = table.find_elements_by_class_name("Section")
+            day = table.find_elements_by_class_name("Day")
+            loc = table.find_elements_by_class_name("Place")
+            prof = table.find_elements_by_class_name("Professor")
+
+
+            #this loop fills the empty lists with there respective elements from the school's website
+            for a, s, d, l, p  in zip(act, sec, day, loc, prof):
+
+                #print(i.text, " Of Type= ",type(i.text))
+                Activity.append(a.text)
+                Section.append(s.text)
+                Day.append(d.text)
+                Prof.append(p.text)
+                Location.append(l.text)
+
+            #loop to split the actual day from time in the Day list
+
+
+            for x in Day:
+                y = x.split(" ")
+                Time.append(y[1]+","+y[-1])
+                tmp = tmp+[y[0]]
+
+            self.Day = tmp[:] # copy tmp's elements
+            #tmp = [] #Set tmp to an empty list
+        
+        '''
        print("_______________________Day______________________________")
         print(Day)
         print("________________________Act_____________________________\n")
@@ -94,10 +96,10 @@ class Course:
 
     
 path = "D:\chromedriver.exe"
-course1 = Course(path, "https://web30.uottawa.ca/v3/SITS/timetable/Course.aspx?id=018041&term=2175&session=A")
+course1 = Course(path, "https://web30.uottawa.ca/v3/SITS/timetable/Course.aspx?id=011209&term=2179&session=FS&yearOfStudy=2")
 print("_____________________________")
 print(course1.Time)
-print(course1.Prof)
+print(course1.Activity)
 print(course1.Day)
 
 
