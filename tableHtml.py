060edaf42350e1b6e57f48b8d719a5449c24761f
brilliant-ember <fire.senfony@gmail.com>
2017-06-10 18:32:36 -0600

    makes the html table and its css file

--- tableHtml.py
+++ tableHtml.py
@@ -1,7 +1,22 @@
 '''This module will reformat the html file exported by pandas and makes it into a pretty table in html format'''
-
+from selenium import webdriver
 
 def htmlChanger(htmlPath):
 	file = open(htmlPath, "+r").readlines()
 	print(file[3])
 
+def firstRowRemover(htmlPath):
+	browser = webdriver.Chrome("D:\chromedriver.exe")
+	page = browser.get(htmlPath)
+	row = page.find_elements_by_tag("tr")
+	for i in range(len(row)):
+		oneRow = row[i]
+		firstCell = oneRow[0]
+		print(firstCell)
+
+
+
+
+path = "http://localhost:8000/filename%20-%20Copy.html"
+
+firstRowRemover(path)
