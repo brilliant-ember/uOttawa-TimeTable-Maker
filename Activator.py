65883d3845e05473acec001ae73ed4e309b7018a
brilliant-ember <fire.senfony@gmail.com>
2017-06-22 14:56:56 -0600

    Activator works on multiple courses

--- Activator.py
+++ Activator.py
@@ -12,44 +12,44 @@ url = "https://web30.uottawa.ca/v3/SITS/timetable/Search.aspx"
 path = "D:\chromedriver.exe"
 #path = r"D:\Programs\phantomjs-2.1.1-windows\bin\phantomjs.exe"
 def urlExtract(L, styleInt):
-	'''takes a list made of [the semester, the rest of courses as strings]'''
-	#browser = webdriver.PhantomJS(path)
-	browser = webdriver.Chrome(path)	
-	browser.get(url)
-	#select = Select(browser.find_element_by_xpath
-	menu = browser.find_element_by_tag_name("select")
-	option = menu.find_elements_by_tag_name("option")
-	input1 = browser.find_element_by_id("ctl00_MainContentPlaceHolder_Basic_SubjectText")
-	input2 = browser.find_element_by_id("ctl00_MainContentPlaceHolder_Basic_CatalogNumberText")
-	submitB = browser.find_element_by_id("ctl00_MainContentPlaceHolder_Basic_Button")
+	for j in range(1, len(L)):
+		'''takes a list made of [the semester, the rest of courses as strings]'''
+		#browser = webdriver.PhantomJS(path)
+		browser = webdriver.Chrome(path)
+		browser.get(url)
+		#select = Select(browser.find_element_by_xpath
+		menu = browser.find_element_by_tag_name("select")
+		option = menu.find_elements_by_tag_name("option")
+		input1 = browser.find_element_by_id("ctl00_MainContentPlaceHolder_Basic_SubjectText")
+		input2 = browser.find_element_by_id("ctl00_MainContentPlaceHolder_Basic_CatalogNumberText")
+		submitB = browser.find_element_by_id("ctl00_MainContentPlaceHolder_Basic_Button")
 
-	semList = ["Fall", "Winter", "Summer"]
-	if L[0] == "f":
-		semster = semList[0]
-	elif L[0] == "w":
-		semster = semList[1]
-	elif L[0] == "s":
-		semster = semList[2]
+		semList = ["Fall", "Winter", "Summer"]
+		if L[0] == "f":
+			semster = semList[0]
+		elif L[0] == "w":
+			semster = semList[1]
+		elif L[0] == "s":
+			semster = semList[2]
 
-	for i in option:
-		if semster in i.text:
-			i.click()
-			break
-	#browser.switch_to.frame("textarea_iframe")
-	wait = WebDriverWait(browser, 10)
-	for i in range(1, len(L)):
-		course = L[i]
+		for i in option:
+			if semster in i.text:
+				i.click()
+				break
+		#browser.switch_to.frame("textarea_iframe")
+		#wait = WebDriverWait(browser, 10)
+		course = L[j]
 		if len(course) ==7:
 
-			# user = wait.until(EC.visibility_of(inputPlace[1]))
+			#user = wait.until(EC.visibility_of(input2))
 			input1.send_keys(course[:3])
 			input2.send_keys(course[3:])
 			submitB.click()
 			link = browser.find_element_by_class_name("CourseCode").find_element_by_tag_name("a").get_attribute('href')
 
 			urlList.append(link)
-	print(urlList)
-	multi(urlList)
+		print(urlList)
+		#multi(urlList)
 
 ##Now the scedule function takes the links list and the style number
 
@@ -61,4 +61,4 @@ def urlExtract(L, styleInt):
 	
 
 
-urlExtract(["w","iti1100"], 5)
+urlExtract(["w","iti1100", "iti1121"], 5)
