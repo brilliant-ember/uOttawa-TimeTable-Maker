1a17016855ad23577c225f57194fc78afb6fa608
brilliant-ember <fire.senfony@gmail.com>
2017-06-24 04:14:50 -0600

    updates Activator

--- Activator.py
+++ Activator.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from Schedule import MultiPossibleTables as multi
 from time import time
+from tkinter import *
 #import threading as T
 import multiprocessing as M
 
@@ -10,13 +11,14 @@ urlList = []
 url = "https://web30.uottawa.ca/v3/SITS/timetable/Search.aspx"
 path = "D:\chromedriver.exe"
 #path = r"D:\Programs\phantomjs-2.1.1-windows\bin\phantomjs.exe"
+
 def urlExtract(L):
+	browser = webdriver.Chrome(path)
+	browser.get(url)
 	for j in range(1, len(L)):
 		'''takes a list made of [the semester, the rest of courses as strings]'''
-		t = time()
+
 		#browser = webdriver.PhantomJS(path)
-		browser = webdriver.Chrome(path)
-		browser.get(url)
 		#select = Select(browser.find_element_by_xpath
 		menu = browser.find_element_by_tag_name("select")
 		option = menu.find_elements_by_tag_name("option")
@@ -24,45 +26,49 @@ def urlExtract(L):
 		input2 = browser.find_element_by_id("ctl00_MainContentPlaceHolder_Basic_CatalogNumberText")
 		submitB = browser.find_element_by_id("ctl00_MainContentPlaceHolder_Basic_Button")
 
-		# semList = ["Fall", "Winter", "Summer"]
-		# if L[0] == "f":
-		# 	semster = semList[0]
-		# elif L[0] == "w":
-		# 	semster = semList[1]
-		# elif L[0] == "s":
-		# 	semster = semList[2]
+		semList = ["Fall", "Winter", "Summer"]
+		if L[0] == "f":
+			semster = semList[0]
+		elif L[0] == "w":
+			semster = semList[1]
+		elif L[0] == "s":
+			semster = semList[2]
 
 		for i in option:
-			semster = "Winter" ###############!!!!!!!!
 			if semster in i.text:
 				i.click()
 				break
-		#browser.switch_to.frame("textarea_iframe")
-		#wait = WebDriverWait(browser, 10)
 		course = L[j]
 		if len(course) ==7:
 
-			#user = wait.until(EC.visibility_of(input2))
 			input1.send_keys(course[:3])
 			input2.send_keys(course[3:])
 			submitB.click()
-			link = browser.find_element_by_class_name("CourseCode").find_element_by_tag_name("a").get_attribute('href')
+			try:
+				link = browser.find_element_by_class_name("CourseCode").find_element_by_tag_name("a").get_attribute('href')
+			except:
+				popup = Tk()
+				popup.title("I found an Error heehe >.< ") 
+				label =Label(popup, text="I couldn't find "+course+", Make sure the course code and semseter are right ", font = ("Vendera", 10, "bold"))
+				label.pack(fill = "x", pady = 10, padx=7)
+				b = Button(popup, text="Destroy?", command = popup.destroy)
+				b.pack(side=BOTTOM, pady=7)
+
+				popup.mainloop()
+
 
 			urlList.append(link)
+			if j != len(L) -1 :
+				browser.get(url)
 	
-	print("from the method!")
+
 	print(urlList)
-	#print("In "+str(time()-t))
-	#multi(urlList)
 
-##Now the scedule function takes the links list and the style number
-
-	#browser.switch_to.default_content()
-
+	##Now toss to the Scedule method
 
 	
 
-t = time()
+
 # t1 = T.Thread(target = urlExtract, args=(["w","iti1100"], 5) )
 # t2 = T.Thread(target = urlExtract, args=(["w","iti1121"], 5) )
 # t1.start()
@@ -89,6 +95,13 @@ def mainMethod(list_of_courses, styleNo):
 	return urlList
 
 
-print(mainMethod(["w","iti1100", "iti1121"], 5))
 
-#urlExtract(["w","iti1100", "iti1121"], 5)
+
+
+
+
+
+#print(mainMethod(["w","iti1100", "iti1121"], 5))
+t = time()
+urlExtract(["w","iti1100", "iti1121"], )
+print("In "+str(time()-t))
