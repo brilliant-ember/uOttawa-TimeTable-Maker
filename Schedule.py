a4c1a056662edf2bb9c62a59caa444d6f5913db5
brilliant-ember <fire.senfony@gmail.com>
2017-06-29 10:51:53 -0600

    works for 2 courses

--- Schedule.py
+++ Schedule.py
@@ -9,12 +9,7 @@ import tableHtml as T
 
 #path = "D:\chromedriver.exe"
 path = r"D:\Programs\phantomjs-2.1.1-windows\bin\phantomjs.exe"
-#course1 = I.Course(path, "http://127.0.0.1:8000/html/ELG2138.html") #circuit theory
-#course2 = I.Course(path, "http://127.0.0.1:8000/html/MAT2322.html")#Calc 3
-#course3 = I.Course(path, "https://web30.uottawa.ca/v3/SITS/timetable/Course.aspx?id=015025&term=2179&session=FS")#engineering mech
-#course4 = I.Course(path, "")
-     
-        
+
 def PossibleTables(one, two):
     '''takes 2 course objs then makes and outputs all possible tables with it, Cartisian product of the tables
     2 courses--->[Tables]'''
@@ -25,7 +20,7 @@ def PossibleTables(one, two):
     grouped_secs = [[x,y]for x in tables_of_1 for y in tables_of_2] #Groups secs based on cartisan product, eg: let course1 hv 2 sections and each section has 2 possible                                                                      tbls, 
                                                                     #and course2 has 2 sections as well with each having 3 possible tbls. what will this do is create this list
                                                                     #[[[all C1 SecA Tbls], [all C2 SecA tbls]], [[all C1 SecA Tbls], [all C2 SecB tbls]]
-
+                                                                    #aka all possible section combinations
             
     #grouped_tables refer to https://stackoverflow.com/questions/3034014/how-to-apply-itertools-product-to-elements-of-a-list-of-lists
     
@@ -44,18 +39,16 @@ def PossibleTables(one, two):
     return output
 
 
-
-
-
-def PossibleTablesVer2(tables_of_1, tables_of_2):
-    '''takes 2 Lists of table obj then makes and outputs all possible tables with it, Cartisian product of the tables
-    2 courses--->[Tables]'''
+def two_possible_tables(tables_of_1, tables_of_2):
+    '''takes 2 doubleLists of tables(each section in a sublist), then makes and outputs all possible tables with it based on Cartisian product of the tables aka all possible combinations
+    2 DLists--->[Tables]'''
 
     output = []
     
     grouped_secs = [[x,y]for x in tables_of_1 for y in tables_of_2] #Groups secs based on cartisan product, eg: let course1 hv 2 sections and each section has 2 possible                                                                      tbls, 
                                                                     #and course2 has 2 sections as well with each having 3 possible tbls. what will this do is create this list
                                                                     #[[[all C1 SecA Tbls], [all C2 SecA tbls]], [[all C1 SecA Tbls], [all C2 SecB tbls]]
+                                                                    #aka all possible section combinations
 
             
     #grouped_tables refer to https://stackoverflow.com/questions/3034014/how-to-apply-itertools-product-to-elements-of-a-list-of-lists
@@ -75,21 +68,14 @@ def PossibleTablesVer2(tables_of_1, tables_of_2):
     return output
 
 
+def table_singular(L):
+    '''takes the double list from I.course.tables and makes that into a single list '''
+    out = []
+    for sec in L :
+        for tbl in sec:
+            out.append(tbl)
+    return out
 
-def MultiPossibleTables(*args):
-    '''This method takes a list of urls and makes a list of courses out of it, then takes these courses and makes all possible tables of them and spits that as a list of tables'''
-    #the *args is a tuple that includes only one list, which is the list of links
-    links = args[0]
-    courses = []
-    print(str(links) + "Im Multi")
-#    for i in links:
-#        print("inital")
-#        courses.append(I.Course(path,i))
-#    
-#    print(len(courses))
-#        
-                       
-                       
 
 def style(I):
     return I
@@ -111,24 +97,59 @@ def formater(tableObj):
     
     return dataMatrix
 
+def MultiPossibleTables(tbls):
+    '''This method takes a list of urls and makes a list of courses out of it, then takes these courses and makes all possible tables of them and spits that as a list of tables'''
+    Out = [] 
+    if len(tbls) == 1:
+        Out.extend(table_singular(tbls[0]))
+        
+    elif len(tbls) ==2 :#we have an even number of tables
+        Out.extend(two_possible_tables(tbls[0], tbls[1]))
+        
+    elif len(tbls) == 3:
+        grouped_secs = list([x,y,z]for x in tbls[0] for y in tbls[1] for z in tbls[2])
+        for i in range(len(grouped_secs)):
+            grouped_tables = list(itertools.product(*grouped_secs[i]))
+            for Atuple in grouped_tables:
+                bool1 = I.isMixable(Atuple[0],Atuple[1])
+                bool2=I.isMixable(Atuple[0],Atuple[2])
+                bool2=I.isMixable(Atuple[1],Atuple[2])
+                
+                if bool1 and bool2 and bool3:
+                    tbl1 = I.tableMixer(Atuple[0], Atuple[1])
+                    oneP = I.tableMixer(tbl1, Atuple[2])
+                    Out.append(oneP)
+        
+    htmlMaker(Out)
+            
+                
+def htmlMaker(Tlist):
+    for i in range(len(Tlist)):
+        tbl = Tlist[i]
+        a = formater(tbl)
+        T.UseThis(a, str(i))
+
 #    
 #
 #ooo = PossibleTables(course1, course2)
 #for i in range(len(ooo)):
 ##    print(i)
-#    print("\n \n \n")
-    #x.print()
-  #  x = ooo[i]
-   # a = formater(x)
-    #print(a)
-  #  T.UseThis(a, str(i))
-      
+#
+#    x = ooo[i]
+#    a = formater(x)
+#    T.UseThis(a, str(i))
+#      
+                
         
-        
-        
+course1 = I.Course(path, "http://127.0.0.1:8000/html/ELG2138.html").tables #circuit theory
+course2 = I.Course(path, "http://127.0.0.1:8000/html/MAT2322.html").tables#Calc 3
+#course3 = I.Course(path, "https://web30.uottawa.ca/v3/SITS/timetable/Course.aspx?id=018041&term=2181&session=FS")#iti1121
+#course4 = I.Course(path, "")
+       
+MultiPossibleTables([course1, course2])
 
 
-                    
+                     
 
 
 
